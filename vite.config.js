91285994d556dd8c5aba884892dde08579d1c774
Lovish <lovish@gkmit.co>
2025-11-23 01:33:05 +0530

    feature: pwa setup

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,45 @@
 
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '') 
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(),
+      VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Vite + React PWA',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+    ],
     server: {
       port: 3000,
       proxy: {
