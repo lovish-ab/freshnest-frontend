1874782348ecdce31b0ee61a3aa6cf32bce11694
Lovish <lovish@gkmit.co>
2025-11-26 11:21:28 +0530

    feature: add seller and user dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import LandingPage from './pages/LandingPage';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import SellerSignUp from './pages/SellerSignUp';
+import SellerDashboard from './pages/SellerDashboard';
+import CustomerDashboard from './pages/CustomerDashboard';
 
 function App() {
   return (
@@ -14,6 +16,22 @@ function App() {
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/seller/signup" element={<SellerSignUp />} />
+            <Route
+              path="/seller/dashboard"
+              element={
+                <ProtectedRoute requiredRole="Seller">
+                  <SellerDashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/customer/dashboard"
+              element={
+                <ProtectedRoute requiredRole="Customer">
+                  <CustomerDashboard />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </Router>
     </AuthProvider>
